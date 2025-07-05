5c64215ec532a20660d3811289debb9d53944fb1
gabcytn <cayetanogabriel03@gmail.com>
2025-07-05 11:24:47 +0800

    initial commit

---
+++ src/main/java/com/gabcytn/shortnotice/ShortnoticeApplication.java
@@ -0,0 +1,13 @@
+package com.gabcytn.shortnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortnoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortnoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabcytn/shortnotice/ShortnoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabcytn.shortnotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortnoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

55de363ae4fa9719d40ac67865116c59e66793e0
gabcytn <cayetanogabriel03@gmail.com>
2025-07-22 22:42:00 +0800

    refactor: use one global exception handler for all exceptions

--- src/main/java/com/gabcytn/shortnotice/Exception/Handler/GlobalExceptionHandler.java
+++ src/main/java/com/gabcytn/shortnotice/Exception/Handler/GlobalExceptionHandler.java
@@ -5,80 +5,93 @@ import com.gabcytn.shortnotice.Exception.RefreshTokenException;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.SignatureException;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import java.util.List;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@Order(Ordered.LOWEST_PRECEDENCE)
 public class GlobalExceptionHandler {
-	@ExceptionHandler(Exception.class)
-	public ProblemDetail handleSecurityException(Exception exception) {
-		ProblemDetail errorDetail = null;
-		exception.printStackTrace();
+  @ExceptionHandler(Exception.class)
+  public ProblemDetail handleSecurityException(Exception exception) {
+    ProblemDetail errorDetail = null;
+    exception.printStackTrace();
 
-		if (exception instanceof BadCredentialsException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
-			errorDetail.setProperty("description", "The username or password is incorrect");
+    if (exception instanceof BadCredentialsException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
+      errorDetail.setProperty("description", "The username or password is incorrect");
 
-			return errorDetail;
-		}
+      return errorDetail;
+    }
 
-		if (exception instanceof AccountStatusException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "The account is locked");
-		}
+    if (exception instanceof AccountStatusException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("description", "The account is locked");
+    }
 
-		if (exception instanceof AccessDeniedException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "You are not authorized to access this resource");
-		}
+    if (exception instanceof AccessDeniedException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("description", "You are not authorized to access this resource");
+    }
 
-		if (exception instanceof SignatureException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "The JWT signature is invalid");
-		}
+    if (exception instanceof SignatureException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("description", "The JWT signature is invalid");
+    }
 
-		if (exception instanceof ExpiredJwtException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "The JWT token has expired");
-		}
+    if (exception instanceof ExpiredJwtException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("description", "The JWT token has expired");
+    }
 
-		if (exception instanceof MalformedJwtException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), exception.getMessage());
-			errorDetail.setProperty("description", "Incorrect token format.");
-		}
+    if (exception instanceof MalformedJwtException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), exception.getMessage());
+      errorDetail.setProperty("description", "Incorrect token format.");
+    }
 
-		if (exception instanceof AuthenticationException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "Failed to authenticate.");
-		}
+    if (exception instanceof AuthenticationException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("description", "Failed to authenticate.");
+    }
 
-		if (exception instanceof RefreshTokenException) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "Your refresh token is invalid.");
-		}
+    if (exception instanceof RefreshTokenException) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+      errorDetail.setProperty("description", "Your refresh token is invalid.");
+    }
 
-		if (errorDetail == null) {
-			errorDetail =
-							ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
-			errorDetail.setProperty("description", "Unknown internal server error.");
-		}
+    if (exception instanceof MethodArgumentNotValidException) {
+      List<String> errors =
+          ((MethodArgumentNotValidException) exception)
+              .getBindingResult().getAllErrors().stream()
+                  .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                  .toList();
 
-		return errorDetail;
-	}
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, "Validation failed.");
+      errorDetail.setProperty("description", errors);
+    }
+
+    if (errorDetail == null) {
+      errorDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
+      errorDetail.setProperty("description", "Unknown internal server error.");
+    }
+
+    return errorDetail;
+  }
 }

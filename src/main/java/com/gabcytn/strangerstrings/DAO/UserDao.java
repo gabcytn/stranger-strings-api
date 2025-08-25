984d346e35a675af9f9d5de64a8c40fb982ed704
gabcytn <cayetanogabriel03@gmail.com>
2025-08-25 20:07:02 +0800

    fix: simple caching for User entity

--- src/main/java/com/gabcytn/strangerstrings/DAO/UserDao.java
+++ src/main/java/com/gabcytn/strangerstrings/DAO/UserDao.java
@@ -3,9 +3,12 @@ package com.gabcytn.strangerstrings.DAO;
 import com.gabcytn.strangerstrings.Entity.User;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDao extends CrudRepository<User, UUID> {
+  @Cacheable(value = "user", key = "#username")
   Optional<User> findByUsername(String username);
 
   Optional<User> findByEmail(String email);
--- src/main/java/com/gabcytn/strangerstrings/Service/UserDetailsServiceAuth.java
+++ src/main/java/com/gabcytn/strangerstrings/Service/UserDetailsServiceAuth.java
@@ -1,14 +1,9 @@
 package com.gabcytn.strangerstrings.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gabcytn.strangerstrings.DAO.RedisCacheDao;
 import com.gabcytn.strangerstrings.DAO.UserDao;
-import com.gabcytn.strangerstrings.DTO.CacheData;
 import com.gabcytn.strangerstrings.DTO.UserPrincipal;
 import com.gabcytn.strangerstrings.Entity.User;
-import java.util.Optional;
+import com.gabcytn.strangerstrings.Exception.UserNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,50 +11,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceAuth implements UserDetailsService {
-  private final UserDao userDAO;
-  private final RedisCacheDao redisCacheDao;
-  private final ObjectMapper objectMapper;
+  private final UserDao userDao;
 
-  public UserDetailsServiceAuth(
-          UserDao userDAO, RedisCacheDao redisCacheDao, ObjectMapper objectMapper) {
-    this.userDAO = userDAO;
-    this.redisCacheDao = redisCacheDao;
-    this.objectMapper = objectMapper;
+  public UserDetailsServiceAuth(UserDao userDao) {
+    this.userDao = userDao;
   }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    Optional<CacheData> cacheData = redisCacheDao.findById(username);
-    if (cacheData.isPresent()) {
-      System.out.println("Cache hit!");
-      TypeReference<User> mapType = new TypeReference<>() {};
-      try {
-        User cachedUser = objectMapper.readValue(cacheData.get().getValue(), mapType);
-        return new UserPrincipal(cachedUser);
-      } catch (JsonProcessingException e) {
-        System.err.println("Error mapping cached string value to user entity");
-        throw new UsernameNotFoundException("Error mapping cached string value to user entity");
-      }
-    }
-
-    System.out.println("Cache miss!");
-    Optional<User> user = userDAO.findByUsername(username);
-    if (user.isPresent()) {
-      User presentUser = user.get();
-      try {
-        redisCacheDao.save(
-            new CacheData(
-                presentUser.getEmail(),
-                objectMapper.writeValueAsString(presentUser),
-                60L * 15)); // cache for 15 mins
-        return new UserPrincipal(presentUser);
-      } catch (JsonProcessingException e) {
-        System.err.println("Error writing user entity as String");
-        throw new UsernameNotFoundException("Error writing user entity as String");
-      }
-    }
-
-    System.err.println("Username/Email not found.");
-    throw new UsernameNotFoundException("Username/Email not found.");
+    User user = userDao.findByUsername(username).orElseThrow(UserNotFoundException::new);
+    return new UserPrincipal(user);
   }
 }

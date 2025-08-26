adcc481ccf95127186896d3c4e8d1a1e047b016f
gabcytn <cayetanogabriel03@gmail.com>
2025-08-26 11:30:12 +0800

    fix: model for refresh token dao

--- src/main/java/com/gabcytn/strangerstrings/Model/RefreshTokenValidator.java
+++ src/main/java/com/gabcytn/strangerstrings/Model/RefreshTokenValidator.java
@@ -1,3 +1,33 @@
-package com.gabcytn.strangerstrings.DTO;
+package com.gabcytn.strangerstrings.Model;
 
-public record RefreshTokenValidatorDto(String username, String deviceName) {}
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@RedisHash(value = "refreshToken")
+public class RefreshTokenValidator {
+  @Id private String username;
+  private String deviceName;
+  @TimeToLive private final long expiresAt = 60L * 60 * 24 * 7;
+
+  public RefreshTokenValidator(String username, String deviceName) {
+    this.username = username;
+    this.deviceName = deviceName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getDeviceName() {
+    return deviceName;
+  }
+
+  public void setDeviceName(String deviceName) {
+    this.deviceName = deviceName;
+  }
+}

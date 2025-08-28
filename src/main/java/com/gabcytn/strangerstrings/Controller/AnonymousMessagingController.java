e46eec09cb68038a6d5a26834f83846bafe77455
gabcytn <cayetanogabriel03@gmail.com>
2025-08-28 15:10:08 +0800

    fix: message mapping & return type of messagingService's match() method

--- src/main/java/com/gabcytn/strangerstrings/Controller/AnonymousMessagingController.java
+++ src/main/java/com/gabcytn/strangerstrings/Controller/AnonymousMessagingController.java
@@ -1,23 +1,23 @@
 package com.gabcytn.strangerstrings.Controller;
 
 import com.gabcytn.strangerstrings.DTO.ChatInitiationDto;
-import com.gabcytn.strangerstrings.DTO.InterestMatchedResponse;
-import com.gabcytn.strangerstrings.Model.MessageServiceQueueingResponse;
+import com.gabcytn.strangerstrings.Model.ConversationMember;
+import com.gabcytn.strangerstrings.Model.QueueMatchedResponse;
 import com.gabcytn.strangerstrings.Service.Interface.MessagingService;
 import jakarta.validation.Valid;
 import java.security.Principal;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/anonymous")
+@MessageMapping("/anonymous")
 public class AnonymousMessagingController {
   private static final Logger LOG = LoggerFactory.getLogger(AnonymousMessagingController.class);
   private final MessagingService messagingService;
@@ -29,17 +29,16 @@ public class AnonymousMessagingController {
     this.simpMessagingTemplate = simpMessagingTemplate;
   }
 
-  @MessageMapping("/match")
+  @MessageMapping("/matcher")
   public void match(@RequestBody @Valid ChatInitiationDto reqBody, Principal principal) {
     try {
-      MessageServiceQueueingResponse response =
+      QueueMatchedResponse<? extends ConversationMember> response =
           messagingService
               .match(reqBody.getInterests(), UUID.fromString(principal.getName()))
               .orElseThrow(RuntimeException::new);
-      InterestMatchedResponse matchedResponse =
-          new InterestMatchedResponse(
-              response.getInterest(), response.getConversationId(), response.getMembers());
-      this.sendConversationDetailsToMatchedUsers(Set.of(), matchedResponse);
+      Set<UUID> memberIds =
+          response.getMembers().stream().map(ConversationMember::getId).collect(Collectors.toSet());
+      this.sendConversationDetailsToMatchedUsers(memberIds, response);
     } catch (RuntimeException e) {
       LOG.info("Waiting a match for: {}", reqBody.getInterests());
     }
@@ -48,7 +47,7 @@ public class AnonymousMessagingController {
   private void sendConversationDetailsToMatchedUsers(Set<UUID> members, Object payload) {
     members.forEach(
         member -> {
-          simpMessagingTemplate.convertAndSendToUser(member.toString(), "/topic/match", payload);
+          simpMessagingTemplate.convertAndSendToUser(member.toString(), "/topic/anonymous/match", payload);
         });
   }
 }

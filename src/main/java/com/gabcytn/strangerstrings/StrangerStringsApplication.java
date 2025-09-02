138621c912f9d616790d8137b5eea3a3e2ec3b21
gabcytn <cayetanogabriel03@gmail.com>
2025-09-02 21:43:14 +0800

    chore: cleanup main function

--- src/main/java/com/gabcytn/strangerstrings/StrangerStringsApplication.java
+++ src/main/java/com/gabcytn/strangerstrings/StrangerStringsApplication.java
@@ -1,35 +1,13 @@
 package com.gabcytn.strangerstrings;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 @EnableCaching
 public class StrangerStringsApplication {
-  private static String USERS_TO_INTERESTS_MAP_REDIS_KEY;
-
   public static void main(String[] args) {
-    ApplicationContext context = SpringApplication.run(StrangerStringsApplication.class, args);
-
-    @SuppressWarnings("unchecked")
-    RedisTemplate<String, Map<String, List<String>>> redisTemplate =
-        context.getBean("redisUsersInterestsMapTemplate", RedisTemplate.class);
-
-    // initialize users-list(interests) map
-    Map<String, List<String>> map = new HashMap<>();
-    map.put("_init", List.of());
-    redisTemplate.opsForValue().set(USERS_TO_INTERESTS_MAP_REDIS_KEY, map);
-  }
-
-  @Value("${spring.data.redis.users-interests-map}")
-  public void setIsUserMatchedMapKey(String key) {
-    USERS_TO_INTERESTS_MAP_REDIS_KEY = key;
+    SpringApplication.run(StrangerStringsApplication.class, args);
   }
 }

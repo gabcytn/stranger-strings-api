0597ff095eaf7f05a6522ee28847802dcd8d8d2f
gabcytn <cayetanogabriel03@gmail.com>
2025-09-03 21:55:15 +0800

    fix: constructors usage and break down main method

--- src/main/java/com/gabcytn/strangerstrings/Controller/MessageRestController.java
+++ src/main/java/com/gabcytn/strangerstrings/Controller/MessageRestController.java
@@ -4,14 +4,14 @@ import com.gabcytn.strangerstrings.DAO.MessageDao;
 import com.gabcytn.strangerstrings.DTO.PaginatedMessageResponse;
 import com.gabcytn.strangerstrings.Entity.Conversation;
 import com.gabcytn.strangerstrings.Entity.Message;
-import com.gabcytn.strangerstrings.Entity.User;
 import com.gabcytn.strangerstrings.Exception.ConversationNotFoundException;
-import com.gabcytn.strangerstrings.Model.AuthenticatedConversationMember;
 import com.gabcytn.strangerstrings.Model.PaginatedMessageContent;
 import com.gabcytn.strangerstrings.Service.ConversationService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/messages")
 public class MessageRestController {
+  private static final Logger LOG = LoggerFactory.getLogger(MessageRestController.class);
   private final MessageDao messageDao;
   private final ConversationService conversationService;
 
@@ -31,35 +32,39 @@ public class MessageRestController {
     this.conversationService = conversationService;
   }
 
-  // FIX: minimize function size (i.e. split up)
   // TODO: handle 'not found' exceptions
   @GetMapping
   public PaginatedMessageResponse get(
       @RequestParam UUID conversationId, @RequestParam int pageNumber) {
     Pageable pageable = PageRequest.of(pageNumber, 10);
-    Conversation conversation =
-        conversationService
-            .getConversation(conversationId)
-            .orElseThrow(ConversationNotFoundException::new);
+    Conversation conversation = this.getConversationById(conversationId);
     Page<Message> messages = messageDao.findByConversation(conversation, pageable);
-    PaginatedMessageResponse response = new PaginatedMessageResponse();
-    response.setPageable(pageable);
 
     List<PaginatedMessageContent> contents = new ArrayList<>();
-    messages.forEach(
-        message -> {
-          PaginatedMessageContent content = new PaginatedMessageContent();
-          content.setId(message.getId());
-          content.setBody(message.getBody());
-          content.setConversationId(message.getConversation().getId());
+    for (Message message : messages) {
+      contents.add(this.createPaginatedMessageContent(message));
+    }
 
-          User sender = message.getSender();
-          content.setSender(sender.getId());
-          content.setDate(message.getCreatedAt());
-          contents.add(content);
-        });
+    return new PaginatedMessageResponse(contents, pageable);
+  }
 
-    response.setContent(contents);
-    return response;
+  private Conversation getConversationById(UUID conversationId) {
+    try {
+      return conversationService
+          .getConversation(conversationId)
+          .orElseThrow(ConversationNotFoundException::new);
+    } catch (ConversationNotFoundException e) {
+      LOG.error("Conversation id not found: {}", conversationId);
+      throw new ConversationNotFoundException();
+    }
+  }
+
+  private PaginatedMessageContent createPaginatedMessageContent(Message message) {
+    return new PaginatedMessageContent(
+        message.getId(),
+        message.getBody(),
+        message.getSender().getId(),
+        message.getConversation().getId(),
+        message.getCreatedAt());
   }
 }
